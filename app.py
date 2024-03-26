9ba14437ea647d1958727a2bb5a601dd021a5afd
Alex Cohen <acohen49@umd.edu>
2024-03-26 15:30:26 -0400

    Created base, favorite, and contact html aling with lots of the code in each for the flask fundamentals assignment

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ def hello_world():  # put application's code here
 @app.route('/hello')
 def hello():  # put application's code here
     return render_template('hello.html')
+@app.route('/favorite-course', methods=["GET","POST"])
+def favorite():  # put application's code here
+    return render_template('favorite-course.html')
+@app.route('/contact', methods=["GET","POST"])
+def contact():  # put application's code here
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 
 @app.route('/about')
 def about():  # put application's code here
